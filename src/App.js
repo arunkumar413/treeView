e4df93befc2e1b945473dcac72f703552ef1846b
arunkumar413 <arunkumar413@gmail.com>
2022-07-26 00:05:41 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
   DeleteIcon,
   EditIcon,
   FilledFolder,
-  FolderNormalIcon
+  FolderNormalIcon,
 } from "./icons";
 import { Test } from "./test";
 
@@ -73,10 +73,19 @@ export default function App() {
   }
 
   function handleDirHover(evt, node) {
+    if (evt.target == evt.currentTarget) {
+      console.log(evt.target.children[0].lastChild.style.display='inline');
+    }
+
     // evt.preventDefault();
     // evt.stopPropagation();
     // debugger;
-    // evt.target.lastChild.style.display = "inline";
+  }
+
+  function handleDirMouseOut(evt, node) {
+    if (evt.target == evt.currentTarget) {
+      console.log(evt.target.children[0].lastChild.style.display='none');
+    }
   }
 
   function buildTree(node, level = 1) {
@@ -90,14 +99,15 @@ export default function App() {
           key={node.id}
           // onMouseEnter={(evt) => handleShowIcons(evt, id)}
           // onMouseLeave={(evt) => hideIcons(evt, id)}
+          onMouseOver={(evt) => handleDirHover(evt, node)}
+          onMouseOut={(evt) => handleDirMouseOut(evt, node)}
           style={{ paddingLeft: level * 10 }}
-          className="dir"
-        >
+          className="dir">
           {/* <FolderNormalIcon /> */}
           <span
-            onMouseOver={(evt) => handleDirHover(evt, node)}
-            className="dir-heading"
-          >
+            // onMouseOver={(evt) => handleDirHover(evt, node)}
+            // onMouseOut={(evt) => handleDirMouseOut(evt, node)}
+            className="dir-heading">
             <span onClick={(evt) => expandNode(evt, node)}> {">"} </span>
             {node.name}
             <span className="icon-container">
@@ -110,8 +120,7 @@ export default function App() {
                 height="16"
                 fill="currentColor"
                 className="bi bi-folder-plus icon"
-                viewBox="0 0 16 16"
-              >
+                viewBox="0 0 16 16">
                 <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z" />
                 <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z" />
               </svg>
@@ -127,8 +136,7 @@ export default function App() {
         <div
           key={uuid()}
           style={{ paddingLeft: level * 10 + 5 }}
-          className="file"
-        >
+          className="file">
           {node.name}
         </div>
       );
